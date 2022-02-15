3fb3f2b198b321fe1e313cf05b421176dc3435c8
Kasper VDAB <kasper.dehondt@vdabcampus.be>
2022-02-15 13:28:00 +0100

    Initial commit

---
+++ src/main/java/be/vdab/meertalig/MeertaligApplication.java
@@ -0,0 +1,13 @@
+package be.vdab.meertalig;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MeertaligApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MeertaligApplication.class, args);
+    }
+
+}
---
+++ src/test/java/be/vdab/meertalig/MeertaligApplicationTests.java
@@ -0,0 +1,13 @@
+package be.vdab.meertalig;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MeertaligApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
